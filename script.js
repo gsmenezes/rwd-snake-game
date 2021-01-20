c25b0045e2376822a4d3ab6784b27079626740f2
gimenezes <menezes.s.gilda@gmail.com>
2021-01-20 16:33:40 -0300

    create food function and directions

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ let box = 32; //tamanho de cada quadrado
 let snake = [];
 snake[0] = {
   x: 8 * box,
-  y: 8 * box
+  y: 8 * box,
 };
 let direction = "right";
+let food = {
+  x: Math.floor(Math.random() * 15 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 1) * box
+};
 
 function createBG() {
   context.fillStyle = "black";
@@ -14,33 +18,53 @@ function createBG() {
 }
 
 function createSnake() {
-  for(i=0; i < snake.length; i++){
+  for (i = 0; i < snake.length; i++) {
     context.fillStyle = "pink";
     context.fillRect(snake[i].x, snake[i].y, box, box);
   }
 }
 
-function startGame(){
+function createFood() {
+  context.fillStyle = "lightblue";
+  context.fillRect(food.x, food.y, box, box);
+}
+
+document.addEventListener("keydown", update);
+
+function update(event) {
+  if (event.keyCode == 37 && direction != "right") direction = "left";
+  if (event.keyCode == 38 && direction != "down") direction = "up";
+  if (event.keyCode == 39 && direction != "left") direction = "right";
+  if (event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+function startGame() {
+  // Faz a cobrinha aparecer novamente na tela quando 'passa' a parede do box
+  if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+  if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+  if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+  if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
   createBG();
   createSnake();
+  createFood();
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
 
-  if(direction == "right") snakeX += box;
-  if(direction == "left") snakeX -= box;
-  if(direction == "up") snakeY -= box;
-  if(direction == "down") snakeY += box;
+  if (direction == "right") snakeX += box;
+  if (direction == "left") snakeX -= box;
+  if (direction == "up") snakeY -= box;
+  if (direction == "down") snakeY += box;
 
   snake.pop();
 
   let newHead = {
-    x : snakeX,
-    y : snakeY
+    x: snakeX,
+    y: snakeY,
   };
 
   snake.unshift(newHead);
 }
 
 let game = setInterval(startGame, 100);
-
